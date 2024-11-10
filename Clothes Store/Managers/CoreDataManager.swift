26be5d227f97f549666493f4c47f95ed108e1a44
Raivis Olehno <raivis.olehno@brighthr.com>
2024-11-10 23:44:31 +0000

    Started work on CoreData

---
+++ Clothes Store/Managers/CoreDataManager.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataManager.swift
+//  Clothes Store
+//
+//  Created by Raivis on 10/11/2024.
+//  Copyright © 2024 RichieHope. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+
+    private init() {}
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ClothesStoreModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Unresolved error \(error)")
+            }
+        }
+        return container
+    }()
+
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
